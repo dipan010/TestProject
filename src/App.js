de29cca9190dc71b8f547077bc5fc4e8805969b2
Dipan Ghosh <dipan.ghosh@hotmail.com>
2020-09-30 14:51:57 +0530

    Technical support incomplete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PortfolioPerformance from './Components/PortfolioPerformance/PortfolioPerformance'
 import './App.css';
+import TechnicalSupport from './Components/TechnicalSupport/TechnicalSupport'
 
 function App() {
     return ( <
         div >
         <
         PortfolioPerformance / >
+        <
+        TechnicalSupport / >
         <
         /div>
     );
---
+++ src/Components/TechnicalSupport/TechnicalSupport.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container,Card, Button, CardGroup } from 'react-bootstrap';
+
+ class TechnicalSupport extends React.Component{
+     render(){
+         return(
+            <Container>
+                <Card>
+                <h3> Technical Support</h3>
+                <hr class="my-3"/>
+                <p class="text-muted"> SALES PROGRESS </p>
+                <div class="row"></div>
+                </Card>
+            </Container>
+         );
+     }
+ }
+
+ export default TechnicalSupport;
